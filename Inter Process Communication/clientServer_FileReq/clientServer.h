4bdbe123bb5d4f49fb0315cb16528d77fe8acafd
Manav Chokshi <106110688+manav4code@users.noreply.github.com>
2023-11-17 20:50:52 +0530

    Update clientServer.h
    Removed unnecessary headers

--- Inter Process Communication/clientServer_FileReq/clientServer.h
+++ Inter Process Communication/clientServer_FileReq/clientServer.h
@@ -5,11 +5,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+#include <string.h> 
 #include <sys/mman.h>
-#include <sys/stat.h>
 #include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
